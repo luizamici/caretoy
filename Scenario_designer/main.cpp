4b10300164767a28ca58c07acaffdae0c22be09f
Luiza Mici <mici@tams129.informatik.uni-hamburg.de>
2013-01-21 17:59:51 +0100

    Little fix: added log message on entering/exiting the application.

--- Scenario_designer/main.cpp
+++ Scenario_designer/main.cpp
@@ -1,6 +1,7 @@
 #include <QtGui>
 #include "ct_scenarioconfig.h"
 #include "ct_logger.h"
+#include <QtDebug>
 
 int main(int argc, char *argv[])
 {
@@ -15,6 +16,14 @@ int main(int argc, char *argv[])
 
 
     Log4Qt::Logger::logger(QLatin1String("main"))->info(
-                "CareToy designer is being executed!");
-    return app.exec();
+                "CareToy scenario designer is being executed!");
+
+    int out = app.exec();
+    if(out == 0)
+    {
+        Log4Qt::Logger::logger(QLatin1String("main"))->info(
+                    "CareToy scenario designer is about to quit!");
+
+    }
+    return out;
 }
