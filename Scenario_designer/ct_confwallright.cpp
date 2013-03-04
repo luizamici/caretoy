29ba9b100773ecc39b436e3112e591574f8fa806
Luiza Mici <mici@tams129.informatik.uni-hamburg.de>
2013-03-04 17:48:55 +0100

    Revert "Revert "Fix""
    This reverts commit 4131b0092df4d5261b4ba4b3167301cdff6c9d6a.

--- Scenario_designer/ct_confwallright.cpp
+++ Scenario_designer/ct_confwallright.cpp
@@ -314,7 +314,16 @@ QString CTConfWallRight::getParameters(QString value){
         stream.writeAttribute("type", "numerical");
         stream.writeCharacters("1");
         stream.writeEndElement();//end condition
-     }
+    }
+    else if(ui->qrb_headlifted_event->isChecked())
+    {
+        stream.writeAttribute("id", "6");
+        stream.writeAttribute("name", "head_lift");
+        stream.writeStartElement("condition");
+        stream.writeAttribute("type","textual");
+        stream.writeCharacters("head up");
+        stream.writeEndElement();//end condition
+    }
 
     stream.writeEndElement(); //end event
 
